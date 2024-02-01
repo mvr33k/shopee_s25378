116ada54dbc51510d3dd7b5db93416cd6303a3d7
mvr3k <61803073+mvr33k@users.noreply.github.com>
2024-02-01 18:08:08 +0100

    Initial commit

---
+++ src/main/java/com/example/shopee_s25378/ShopeeS25378Application.java
@@ -0,0 +1,13 @@
+package com.example.shopee_s25378;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeS25378Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeS25378Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shopee_s25378/ShopeeS25378ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopee_s25378;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeS25378ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
