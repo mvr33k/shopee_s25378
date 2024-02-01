d71d47bc61ff3a14fe3adedc31e281d3b4494580
mvr3k <61803073+mvr33k@users.noreply.github.com>
2024-02-01 20:12:34 +0100

    koniec(?)

--- src/test/java/com/example/shopee_s25378/ShopServiceIntegrationTest.java
+++ src/test/java/com/example/shopee_s25378/ShopServiceIntegrationTest.java
@@ -1,6 +1,41 @@
 package com.example.shopee_s25378;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+@SpringBootTest
 public class ShopServiceIntegrationTest {
+    @MockBean
+    private ShopStorage shopStorage;
+    @Autowired
+    private ShopService shopService;
+
+
+    @Test
+    void shouldFindByName(){
+        ShopProduct product1=new ShopProduct("piwo",2.50);
+        ShopProduct product2=new ShopProduct("mleko", 2.00);
+        List<ShopProduct> productList=new ArrayList<>();
+        productList.add(product1);
+        productList.add(product2);
+
+        when(shopStorage.getAll()).thenReturn(productList);
+
+        assertThat(shopService.isProductInStorage("piwo")).isTrue();
+    }
+
+//    shouldNot
 
 
 }
--- src/test/java/com/example/shopee_s25378/ShopServiceTest.java
+++ src/test/java/com/example/shopee_s25378/ShopServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
+
 public class ShopServiceTest {
 
     private ShopService shopService;
